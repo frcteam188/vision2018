8890c269f346be0f28822064b533d06969548b2c
Vanshil Shah <mr.vanshilshah@gmail.com>
2019-02-07 18:51:38 -0500

    display vision in startup

--- vision_start.sh
+++ vision_start.sh
@@ -1,4 +1,4 @@
 source /usr/local/bin/virtualenvwrapper.sh
 workon cv
 v4l2-ctl -c exposure_auto=1 -c exposure_absolute=5
-python ~/Dev/vision2019/threading_track.py
+python ~/Dev/vision2019/threading_track.py -d 1
