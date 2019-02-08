197625b72b8d4f78a7913defebbefda6445773c0
Vanshil Shah <mr.vanshilshah@gmail.com>
2019-02-07 21:00:57 -0500

    fix

--- vision_start.sh
+++ vision_start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 source /usr/local/bin/virtualenvwrapper.sh
-workon vision2018
+workon cv
 v4l2-ctl -c exposure_auto=1 -c exposure_absolute=5
 python ~/Dev/vision2019/threading_track.py -d 1
