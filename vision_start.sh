491c8f016951a82ed1cf3b99aa8807a871cc9523
Vanshil Shah <mr.vanshilshah@gmail.com>
2019-02-09 12:56:25 -0500

    used home directory

--- vision_start.sh
+++ vision_start.sh
@@ -3,4 +3,4 @@
 source /usr/local/bin/virtualenvwrapper.sh
 workon cv
 v4l2-ctl -c exposure_auto=1 -c exposure_absolute=5
-python ~/Dev/vision2019/threading_track.py
+python /home/pi/Dev/vision2019/threading_track.py
